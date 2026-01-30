584253b24364c349001582f615d2883234fe4869
Joonatan Halme <joonatanhalme@gmail.com>
2026-01-30 16:38:48 +0200

    started ui

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ from __future__ import print_function
 from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
 import keyboard
 import json
+import sys
+import time
+from tkinter import *
+from tkinter import ttk
 
-volumeUp = "ctrl+å"
-volumeDown = "ctrl+ä"
+#Create window
+root = Tk()
+root.title("Volume controller")
+root.geometry("520x280")
 
 #Get audio sessions
 sessions = AudioUtilities.GetAllSessions()
-
-def readHotkeys():
-    with open('hotkeys.json', 'r') as hotkeys:
-        binds = json.load(hotkeys)
-    print(binds["Spotify.exe"])
 
 def getAppAudioStartLevel(appName):
      sessions = AudioUtilities.GetAllSessions()
@@ -21,6 +22,7 @@ def getAppAudioStartLevel(appName):
         if session.Process and session.Process.name() == appName:
             return volumeLevel.GetMasterVolume()
 
+#Change apps audio
 def changeAppAudio(appName, volumeChange):
     sessions = AudioUtilities.GetAllSessions()
     for session in sessions:
@@ -32,14 +34,65 @@ def changeAppAudio(appName, volumeChange):
             currentAppVol.SetMasterVolume(newVol, None)
             print(f"volume: {currentAppVol.GetMasterVolume()}")
 
-def increaseVolume():
-    changeAppAudio("Spotify.exe", 0.05)
+def increaseVolume(appName):
+    changeAppAudio(appName, 0.05)
 
-def decreaseVolume():
-    changeAppAudio("Spotify.exe", -0.05)
+def decreaseVolume(appName):
+    changeAppAudio(appName, -0.05)
+     
+def readHotkeys():
+    with open('hotkeys.json', 'r') as hotkeys:
+        binds = json.load(hotkeys)
+        print(binds)
 
-keyboard.add_hotkey(volumeUp, increaseVolume)
-keyboard.add_hotkey(volumeDown, decreaseVolume)
-keyboard.wait()
+    for app_name, actions in binds.items():
+        for action, keybind in actions.items():
+            if action == "VolumeUp":
+                keyboard.add_hotkey(keybind, lambda: increaseVolume(app_name))
+            
+            if action == "VolumeDown":
+                keyboard.add_hotkey(keybind, lambda: decreaseVolume(app_name))
+            print(action, keybind, app_name)
+
+#Info Text
+infoText = Label(root, text="Select an app to set Hotkey", font='Helvetica 14 bold')
+infoText.grid(row=0, column=0, padx=20, pady=5)
+
+#Get active List item
+hkEntry = Entry(root)
+hkEntry.grid(row=1, column=1)
+
+#Set hotkey in hotkeys.json
+#  "!!!!!!fix utf-8!!!!!!"
+def setHotkey():
+    selectedHotkey = hkEntry.get()
+    print(selectedHotkey)
+
+#Get selected app
+def getSelectedApp():
+    selectedApp = openApps.get()
+    if selectedApp != "Select an app":
+        print(selectedApp)
+        setHotkey()
+
+#List all apps with audio
+Apps = []
+for session in sessions:
+    if session.Process and session.Process.name():
+        Apps.append(session.Process.name())
+        opt = StringVar(value=session.Process.name())
+
+openApps = ttk.Combobox(root, state='readonly', values=Apps)
+openApps.set("Select an app")
+openApps.grid(row=1, column=0, pady=20)
+
+applyBtn = Button(root, text="Apply", font='Helvetica 14 bold', relief='solid', command=getSelectedApp)
+applyBtn.grid(row=0, column=1, pady=20)
+
+
+#Exit Keybind
+root.bind("<Control-Alt-BackSpace>", sys.exit)
 
 readHotkeys()
+
+root.mainloop()
